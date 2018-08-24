791d878280935e4f805a555abbd14a0c41ff759d
Daniel Ortega <contact@daniel-ortega.mx>
2018-08-23 21:25:31 -0500

    vue integration

--- system.conf/boot.sh
+++ system.conf/boot.sh
@@ -1,1 +1,1 @@
-export PATH=$PATH:~/projects/github.com/gatero/.system/system.conf/bin:~/projects/github.com/gatero/.system/system.conf/bin/rar
+export PATH=$PATH:~/projects/github.com/gatero/.system/system.conf/bin:~/projects/github.com/gatero/.system/system.conf/bin/rar:~/.local/bin
