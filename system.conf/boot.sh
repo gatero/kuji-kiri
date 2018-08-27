0fcad50197eecb75e9bd248d3b6b44c6d048f54c
Daniel Ortega <contact@daniel-ortega.mx>
2018-08-26 20:12:34 -0500

    change vim theme

--- system.conf/boot.sh
+++ system.conf/boot.sh
@@ -1,1 +1,3 @@
 export PATH=$PATH:~/projects/github.com/gatero/.system/system.conf/bin:~/projects/github.com/gatero/.system/system.conf/bin/rar:~/.local/bin
+
+code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
