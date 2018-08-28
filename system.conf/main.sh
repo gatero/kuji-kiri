52dd75ba2d923ff31d19f2e25d54558271f0f313
Daniel Ortega <contact@daniel-ortega.mx>
2018-08-27 20:58:52 -0500

    remove unnecessary echoes

--- system.conf/main.sh
+++ system.conf/main.sh
@@ -1,5 +1,3 @@
-echo 'loading environment...'
 source ~/projects/github.com/gatero/.system/system.conf/variables.sh;
 source ~/projects/github.com/gatero/.system/system.conf/alias.sh;
 source ~/projects/github.com/gatero/.system/system.conf/boot.sh;
-echo 'success !!'
